ae1be2cf44923f28a508b550ccf1d206656078b5
Saunders1732 <RSaunders1732@yahoo.com>
2015-11-13 21:35:51 -0800

    moved to sub dir

---
+++ Models/PoductModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TCubDash.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ProductModel 
+    {
+        public string Category { get; set; }
+
+        public int PackSize { get; set; }
+
+        public int QtyPerUnit { get; set; }
+
+        public int QtyInstock { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public string CurrencyTypeName { get; set; }  //System Entity stores Currency Id
+
+        public string MfgPartNo { get; set;  }  //SKU
+
+        public string VendorPartNo { get; set; } //unique number from supplier
+
+        public string VendorName { get; set; } //System Entity stores Vendor Id
+
+
+        public string ImageName { get; set; } //DB type TEXT
+
+        public string ProductDescription { get; set; } //DB type TEXT
+
+        public string ProductSheet { get; set; }
+        
+        public string WebSite { get; set; }
+        
+
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TCubDash")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TCubDash")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07bd66c6-b23e-455f-af16-bdc9a9db5a54")]
